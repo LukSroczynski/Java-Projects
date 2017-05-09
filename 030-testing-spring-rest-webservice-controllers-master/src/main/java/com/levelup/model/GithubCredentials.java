d7856f79cfd7fc4b285590e509334696f468c86d
Surreallistic <iluzion5@o2.pl>
2017-05-09 22:36:10 +0200

    half task

---
+++ 030-testing-spring-rest-webservice-controllers-master/src/main/java/com/levelup/model/GithubCredentials.java
@@ -0,0 +1,20 @@
+package com.levelup.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Lukasz S. on 08.05.2017.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GithubCredentials {
+
+    private String owner;
+
+    private String repositoryName;
+
+}
